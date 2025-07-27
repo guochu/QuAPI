571da736735bfd7ece412b3300d9dbe36add0437
guo chu <guochu604b@gmail.com>
2025-07-27 22:39:35 +0800

    gc changes

--- src/bcs/mixedtime.jl
+++ src/bcs/mixedtime.jl
@@ -1,6 +1,6 @@
 Δm(bath::AbstractBCSBath; Nτ::Int, t::Real, Nt::Int, δτ::Real=bath.β/Nτ) = bcs_Δm(bath.spectrum, β=bath.β, μ=bath.μ, Δ=bath.Δ, Nτ=Nτ, t=t, Nt=Nt, δτ=δτ)
 
-function bcs_Δm(f::AbstractBoundedFunction; β::Real, Nτ::Int, t::Real, Nt::Int, Δ::Real=0, μ::Real=0, δτ::Real=β/Nτ)
+function bcs_Δm(f::AbstractBoundedFunction; β::Real, Nτ::Int, t::Real, Nt::Int, Δ::Number=0, μ::Real=0, δτ::Real=β/Nτ)
 	fu2, fuv, fvu, fv2 = get_all_fs(f, Δ)
 	Δτ_uu = fermionic_Δm(fu2, β=β, Nτ=Nτ, t=t, Nt=Nt, μ=μ, δτ=δτ)
 	Δτ_uv = fermionic_Δm(fuv, β=β, Nτ=Nτ, t=t, Nt=Nt, μ=μ, δτ=δτ)
--- src/bcs/realtime.jl
+++ src/bcs/realtime.jl
@@ -1,7 +1,7 @@
 Δt(bath::AbstractBCSBath; N::Int, t::Real) = bcs_Δt(bath.spectrum, β=bath.β, μ=bath.μ, Δ=bath.Δ, N=N, t=t)
 
 
-function bcs_Δt(f::AbstractBoundedFunction; β::Real, N::Int, t::Real, Δ::Real=0, μ::Real=0) 
+function bcs_Δt(f::AbstractBoundedFunction; β::Real, N::Int, t::Real, Δ::Number=0, μ::Real=0) 
 	fu2, fuv, fvu, fv2 = get_all_fs(f, Δ)
 	Δt_uu = fermionic_Δt(fu2, β=β, N=N, t=t, μ=μ)
 	Δt_uv = fermionic_Δt(fuv, β=β, N=N, t=t, μ=μ)
