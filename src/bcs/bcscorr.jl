621ef56393f657126053867c64e9df1f34a071d1
guo chu <guochu604b@gmail.com>
2026-01-09 10:29:00 +0800

    minor changes

--- src/bcs/bcscorr.jl
+++ src/bcs/bcscorr.jl
@@ -32,6 +32,17 @@ function _vu(ϵ, Δ)
 	return conj(_v(ϵ, Δ)) * _u(ϵ, Δ)
 end
 
+"""
+	struct BCSCorrelationFunction
+
+Exponent of Feynman-Vernon IF is:
+ā↑ Δ↑↑ a↑ + ā↑ Δ↑↓ ā↓ + a↓ Δ↓↑ a↑ + a↓ Δ↓↓ ā↓
+
+uu <-> cn
+ud <-> cc
+du <-> nn
+dd <-> nc
+"""
 struct BCSCorrelationFunction{C <: AbstractCorrelationFunction}
 	cc::C
 	cn::C
@@ -39,13 +50,47 @@ struct BCSCorrelationFunction{C <: AbstractCorrelationFunction}
 	nn::C
 end
 
-function Base.getindex(x::BCSCorrelationFunction, i::Int, j::Int)
-	(1 <= i <= 2) || throw(BoundsError(1:2, i))
-	(1 <= j <= 2) || throw(BoundsError(1:2, j))
-	return ifelse(i==1, ifelse(j==1, x.cc, x.cn), ifelse(j==1, x.nc, x.nn))
-end
+# function Base.getindex(x::BCSCorrelationFunction, i::Int, j::Int)
+# 	(1 <= i <= 2) || throw(BoundsError(1:2, i))
+# 	(1 <= j <= 2) || throw(BoundsError(1:2, j))
+# 	return ifelse(i==1, ifelse(j==1, x.cc, x.cn), ifelse(j==1, x.nc, x.nn))
+# end
 Base.getindex(x::BCSCorrelationFunction, c1::Bool, c2::Bool) = ifelse(c1, ifelse(c2, x.cc, x.cn), ifelse(c2, x.nc, x.nn))
+function BCSCorrelationFunction(; uu::AbstractCorrelationFunction, ud::AbstractCorrelationFunction, du::AbstractCorrelationFunction, dd::AbstractCorrelationFunction)
+	return BCSCorrelationFunction(ud, uu, dd, du)
+end 
 
+function Base.getindex(x::BCSCorrelationFunction, c1::Symbol, c2::Symbol)
+	(c1 in (:↑, :↓)) || throw(ArgumentError("c1 must be :↑ or :↓"))
+	(c2 in (:↑, :↓)) || throw(ArgumentError("c2 must be :↑ or :↓"))
+	if c1 == :↑
+		if c2 == :↑
+			return x.uu
+		else
+			return x.ud
+		end
+	else
+		if c2 == :↑
+			return x.du
+		else
+			return x.dd
+		end
+	end
+end
+
+function Base.getproperty(m::BCSCorrelationFunction, s::Symbol)
+	if s == :uu
+		return m.cn
+	elseif s == :ud
+		return m.cc
+	elseif s == :du
+		return m.nn
+	elseif s == :dd
+		return m.nc
+	else
+		return getfield(m, s)
+	end
+end
 
 
 _mult_f(x::AbstractBoundedFunction, f) = similar(x, w -> f(w) * x.f(w)) 
--- test/bcs.jl
+++ test/bcs.jl
@@ -1,54 +1,55 @@
-# println("------------------------------------")
-# println("|           BCS Δτ Δt Δm           |")
-# println("------------------------------------")
+println("------------------------------------")
+println("|           BCS Δτ Δt Δm           |")
+println("------------------------------------")
 
 
-# @testset "Imaginary time" begin
-# 	atol = 1.0e-8
-# 	β = 5
-# 	for f in (Leggett(), DiracDelta(ω=0.5))
-# 		for μ in (-0.3, 0, 0.3)
-# 			corr1 = fermionic_Δτ(f, β=β, N=10, μ=μ)
-# 			corr2 = bcs_Δτ(f, β=β, N=10, μ=μ, Δ=complex(0))
-# 			@test corr1 ≈ corr2[true, false] atol=atol
-# 			@test corr1 ≈ -transpose(corr2[false, true]) atol=atol
-# 			@test iszero(corr2[true, true])
-# 			@test iszero(corr2[false, false])
-# 		end
-# 	end
-# end
+@testset "Imaginary time" begin
+	atol = 1.0e-8
+	β = 5
+	μ = 0
+	for f in (Leggett(), DiracDelta(ω=0.5))
+		corr1 = fermionic_Δτ(f, β=β, N=10, μ=μ)
+		corr2 = bcs_Δτ(f, β=β, N=10, μ=μ, Δ=complex(0))
+		@test corr2[true, true] === corr2[:↑, :↓] === corr2.ud === corr2.cc
+		@test corr2[true, false] === corr2[:↑, :↑] === corr2.uu === corr2.cn
+		@test corr2[false, true] === corr2[:↓, :↓] === corr2.dd === corr2.nc
+		@test corr2[false, false] === corr2[:↓, :↑] === corr2.du === corr2.nn
+		@test corr1 ≈ corr2[true, false] atol=atol
+		@test corr1 ≈ -transpose(corr2[false, true]) atol=atol
+		@test iszero(corr2[true, true])
+		@test iszero(corr2[false, false])
+	end
+end
 
 
 
-# @testset "Real time" begin
-# 	β = 5
-# 	for f in (semicircular(), DiracDelta(ω=0.5))
-# 		for μ in (-0.3, 0, 0.3)
-# 			x = fermionic_Δt(f, β=β, t=2, N=10, μ=μ)
-# 			y = bcs_Δt(f, β=β, t=2, N=10, μ=μ)
+@testset "Real time" begin
+	atol = 1.0e-8
+	β = 5
+	μ = 0
+	for f in (semicircular(), DiracDelta(ω=0.5))
+		x = fermionic_Δt(f, β=β, t=2, N=10, μ=μ)
+		y = bcs_Δt(f, β=β, t=2, N=10, μ=μ)
 
-# 			@test x ≈ y[true, false] atol=1.0e-8
-# 			@test x ≈ -transpose(y[false, true]) atol=1.0e-8
-# 			@test iszero(y[true, true])
-# 			@test iszero(y[false, false])
-
-# 		end
-# 	end
-# end
+		@test x ≈ y[true, false] atol=atol
+		@test x ≈ -transpose(y[false, true]) atol=atol
+		@test iszero(y[true, true])
+		@test iszero(y[false, false])
+	end
+end
 
 
-# @testset "Mixed time" begin
-# 	β = 2
-# 	for f in (semicircular(), DiracDelta(ω=0.5))
-# 		for μ in (-0.3, 0, 0.3)
-# 			x = fermionic_Δm(f, β=β, t=2, Nt=4, Nτ=5, μ=μ)
-# 			y = bcs_Δm(f, β=β, t=2, Nt=4, Nτ=5, μ=μ)
+@testset "Mixed time" begin
+	atol = 1.0e-8
+	β = 2
+	μ = 0
+	for f in (semicircular(), DiracDelta(ω=0.5))
+		x = fermionic_Δm(f, β=β, t=2, Nt=4, Nτ=5, μ=μ)
+		y = bcs_Δm(f, β=β, t=2, Nt=4, Nτ=5, μ=μ)
 
-# 			@test x ≈ y[true, false] atol=1.0e-8
-# 			@test x ≈ -transpose(y[false, true]) atol=1.0e-8
-# 			@test iszero(y[true, true])
-# 			@test iszero(y[false, false])
-
-# 		end	
-# 	end
-# end
+		@test x ≈ y[true, false] atol=atol
+		@test x ≈ -transpose(y[false, true]) atol=atol
+		@test iszero(y[true, true])
+		@test iszero(y[false, false])
+	end
+end
